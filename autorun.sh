bbda0bfcb1553039af3eadd0bc3158b729e52076
Jairo Salazar Vázquez <jairosalazarvazquez@gmail.com>
2017-06-13 16:50:23 +0000

    Merging

--- autorun.sh
+++ autorun.sh
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
 cd /home/pi/Documents/RaspHypCam/
+clear
 ./HypRaspCam 51717
-=======
-cd /home/pi/Documents/HypCam/
-./HypRaspCam 51717
-
->>>>>>> e15b9a41b7d3ecda26d4a5adafb9664d8fbe84e7
