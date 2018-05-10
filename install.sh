84ace73016d7d6da8435bc176fbce7818ae47b8f
JairoSalazarV <jairosalazarvazquez@gmail.com>
2018-05-09 19:54:15 -0500

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,15 @@
+git clone --depth=1 https://github.com/JairoSalazarV/RaspHypCam
+
+cd RaspHypCam/
+
+mkdir tmpSlideLapse tmpSnapshots tmpTimeLapse tmpVideos
+
+g++ HypRaspCam.cpp -o HypRaspCam
+
+sudo cp HypCamAutorun /etc/init.d/RaspHypCam
+
+sudo chmod 755 /etc/init.d/RaspHypCam
+
+sudo update-rc.d RaspHypCam defaults
+
+sudo /etc/init.d/RaspHypCam start
